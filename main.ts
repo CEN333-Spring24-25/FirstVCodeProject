4ecc92ffe453cf57583c07ca2adcb834b27a08b1
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2025-03-06 20:52:08 +0400

    semicolon

--- main.ts
+++ main.ts
@@ -6,4 +6,4 @@ let myPatient: Patient = new Patient(784, "Mohammed Ghazal", 42);
 myPatient.addMedication("Panadol", 500, "mg");
 myPatient.addMedication("Concur", 5, "mg");
 
-console.log(myPatient.toString())
+console.log(myPatient.toString());
