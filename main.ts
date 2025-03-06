e9cc62947a5f8adfa951282b2863d4f8234016c1
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2025-03-06 20:59:27 +0400

    A comment was added

--- main.ts
+++ main.ts
@@ -7,3 +7,5 @@ myPatient.addMedication("Panadol", 500, "mg");
 myPatient.addMedication("Concur", 5, "mg");
 
 console.log(myPatient.toString());
+
+// Thank you so much. Please learn from this and use it to solve problems one and two. Good luck. 
